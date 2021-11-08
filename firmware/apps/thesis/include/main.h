b729e368a1853b65ce5421acf459492967157992
lainslie <l.ainslie@outlook.com>
2021-11-08 14:12:49 +1000

    change format

--- firmware/apps/thesis/include/main.h
+++ firmware/apps/thesis/include/main.h
@@ -1,9 +1,9 @@
 /**   
  ***************************************************************
- * @file    apps/p3/include/main.h   
+ * @file    apps/thesis/include/main.h   
  * @author  Lewis Ainslie - s4485827  
- * @date    08/03/2021   
- * @brief   Main header file for including    
+ * @date    08/11/2021   
+ * @brief   Main header file for includes    
  ***************************************************************
  */
 
@@ -16,22 +16,11 @@
 #include <devicetree.h>
 #include <init.h>
 #include <drivers/gpio.h>
-
 #include <drivers/uart.h>
-
-
-
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-
-// extern struct shell* shell_used;
-
-// #include <shell/shell_uart.h>
-
-
 
 #endif
